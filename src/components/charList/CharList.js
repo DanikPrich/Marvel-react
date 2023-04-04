a33c6888b556da651bb5d7af2a100bcb349a2141
Daniel <Danil.danek@gmail.com>
2023-04-04 20:54:03 +0200

    CharList to hooks

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types'
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -6,83 +6,72 @@ import MarvelService from '../../services/MarvelService';
 
 import './charList.scss';
 
-class CharList extends Component{
-    state = {
-        charList: [],
-        loading: true,
-        error: false,
-        newItemLoading: false,
-        offset: 1541, 
-        charEnded: false,
-    }
+const CharList = (props) => {
 
-    marvelService = new MarvelService();
+    const [charList, setCharList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(1541);
+    const [charEnded, setCharEnded] = useState(false);
+
+    const marvelService = new MarvelService();
 
     /* Когда замаунтился компонент */
-    componentDidMount() {
-        this.onRequest()
-    }
+    useEffect(() => {
+        onRequest();
+    }, [])
     
     /* Метод который отвечает за запрос на сервер, первый раз вызывается при рендере */
-    onRequest = (offset) => {
+    const onRequest = (offset) => {
         /* В первый раз вызова это ни на что не влияет, потом же мы будем кидать с помощью этого метода атрибут кнопке disabled */
-        this.onCharListLoading();
+        onCharListLoading();
         /* Отправляем запрос */
-        this.marvelService
+        marvelService
         .getAllCharacters(offset)
         /* Записываем новые поля в стейт */
-        .then(this.onCharactersLoaded)
+        .then(onCharactersLoaded)
         /* Краш */
-        .catch(this.onError)
+        .catch(onError)
     }
     
-    onCharListLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
+   const onCharListLoading = () => {
+        setNewItemLoading(true);
     }
     
     /* Когда загрузился, записываем новых персон в стейт */
-    onCharactersLoaded = (newCharList) => {
+    const onCharactersLoaded = (newCharList) => {
         let ended = false;
         if (newCharList.length < 9) ended = true;
-
+        
         /* Записываем в стейт относительно старого стейта */
-        this.setState(({offset, charList}) => ({
-            /* Конкатинируем старый массив персонажей и добавляем новых */
-            charList: [...charList, ...newCharList],
-            /* Поля записались, выключаем загрузку */
-            loading: false,
-            newItemLoading: false,
-            offset: offset + 9,
-            charEnded: ended
-        }))
+        /* Конкатинируем старый массив персонажей и добавляем новых */
+        setCharList(charList => [...charList, ...newCharList]);
+        /* Поля записались, выключаем загрузку */
+        setLoading(loading => false);
+        setNewItemLoading(newItemLoading => false)
+        setOffset(offset => offset + 9)
+        setCharEnded(charEnded => ended)
     }
     /* При ерроре показываем еррор и останавливаем загрузку */
-    onError = () => {
-        this.setState({
-            charList: [],
-            error: true,
-            loading: false
-        })
+    const onError = () => {
+        setError(true);
+        setLoading(false);
     }
 
     /* Создаем массив пустой рефов */
-    charRefs = [];
+    const charRefs = useRef([])
 
-    /* Пушим в массив рефы айтемов */
-    setCharRefs = (elem) => {
-        this.charRefs.push(elem);
-    }
+
 
     /* Удаляем всем актив стиль и добавляем нужному, ставим фокус */
-    focusOnItem = (i) => {
-        this.charRefs.forEach(item => item.classList.remove('char__item_selected'))
-        this.charRefs[i].classList.add('char__item_selected')
-        this.charRefs[i].focus(); 
+    const focusOnItem = (i) => {
+        charRefs.current.forEach(item => item.classList.remove('char__item_selected'))
+        charRefs.current[i].classList.add('char__item_selected')
+        charRefs.current[i].focus(); 
     }
 
-    renderItems = (charList) => {
+    const renderItems = (charList) => {
         /* Создаем новый массив на базе прежних */
         const items = charList.map((item,i) => {
             /* Если фото не найдено, изамени отображение картинки */
@@ -93,23 +82,24 @@ class CharList extends Component{
             return (
                 /* Вернем массив из элементов */
                 <li className="char__item" 
+                /* Пушим в массив рефы айтемов */
+                ref={el => charRefs.current[i] = el}
                 key={item.id}
                 /* При клике ставим фокус и вызываем функцию из пропса */
                 onClick={() => {
-                    this.props.onCharSelected(item.id);
-                    this.focusOnItem(i)
+                    props.onCharSelected(item.id);
+                    focusOnItem(i)
                 }}
                 /* При нажатии ентера или е ставим фокус и вызываем ф из пропса */
                 onKeyDown={(e) => {
                     if(e.key === "Enter" || e.key === 'e') {
-                        this.props.onCharSelected(item.id); 
-                        this.focusOnItem(i);
+                        props.onCharSelected(item.id); 
+                        focusOnItem(i);
                     }
                 }}
                 /* Чтобы можно было через таб перемещаться к элементу */
                 tabIndex={0}
                 /* Вызываем функцию рефов и передаем выбранный элемент */
-                ref={this.setCharRefs}
                 >
                     <img src={item.thumbnail} alt="abyss" style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
@@ -124,29 +114,26 @@ class CharList extends Component{
         )
     }
 
-    render() {
-        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state
+    const items = renderItems(charList)
 
-        const items = this.renderItems(charList)
+    const spinner = loading ? <Spinner/> : null
+    const errorMessage = error ? <ErrorMessage/> : null
+    const content = !(loading || error) ? items : null
 
-        const spinner = loading ? <Spinner/> : null
-        const errorMessage = error ? <ErrorMessage/> : null
-        const content = !(loading || error) ? items : null
-        return (
-            <div className="char__list">
-                {spinner}
-                {errorMessage}
-                {content}
-                <button 
-                className="button button__main button__long"
-                disabled={newItemLoading}
-                style={{'display' : charEnded ? 'none' : 'block'}}
-                onClick={() => this.onRequest(offset)}>
-                    <div className="inner">load more</div>
-                </button>
-            </div>
-        )
-    }
+    return (
+        <div className="char__list">
+            {spinner}
+            {errorMessage}
+            {content}
+            <button 
+            className="button button__main button__long"
+            disabled={newItemLoading}
+            style={{'display' : charEnded ? 'none' : 'block'}}
+            onClick={() => onRequest(offset)}>
+                <div className="inner">load more</div>
+            </button>
+        </div>
+    )
 }
 
 /* Проверяем пропс onCharSelected на обязательную функцию */
