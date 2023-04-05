be387359dfbd256d08002789261413c8d78aa1bc
Daniel <Danil.danek@gmail.com>
2023-04-05 18:55:13 +0200

    Service file rebuilded to hook

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,36 +1,38 @@
+import { useHttp } from "../hooks/http.hook";
+
+const useMarvelService = () => {
+  const {loading, request, error} = useHttp();
+
+  const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  const _apiKeyTemp = 'apikey=81f3f697d7f1277aea82c579fd4a1eab';
+  const _apiKey = 'apikey=a4fdd89c59c787a3f9c7e4daf01647a8';
+  const _baseOffset = 210;
 
 
-class MarvelService {
-  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiTemp = 'apikey=81f3f697d7f1277aea82c579fd4a1eab';
-  _apiKey = 'apikey=a4fdd89c59c787a3f9c7e4daf01647a8';
-  _baseOffset = 210;
+//  getResource = async (url) => {
+//     let res = await fetch(url);
 
+//     if (!res.ok) {
+//       throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+//     }
 
- getResource = async (url) => {
-    let res = await fetch(url);
+//     return await res.json();
+//   }
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    }
-
-    return await res.json();
+  const getAllCharacters = async (offset = _baseOffset) => {
+    const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKeyTemp}`);
+    /* Создаем массив с удобными для нас обьектами */
+    return res.data.results.map(_transformCharacter);
   }
 
-  getAllCharacters = async(offset = this._baseOffset) => {
-    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiTemp}`);
+  const getCharacter = async (id) => {
+     const res = await request(`${_apiBase}characters/${id}?${_apiKeyTemp}`);
     /* Создаем массив с удобными для нас обьектами */
-    return res.data.results.map(this._transformCharacter);
-  }
-
-  getCharacter = async (id) => {
-     const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiTemp}`);
-    /* Создаем массив с удобными для нас обьектами */
-     return this._transformCharacter(res.data.results[0]);
+     return _transformCharacter(res.data.results[0]);
   }
 
   /* Метод который переводит неудобные данные в удобный обьект */
-  _transformCharacter = (char) => {
+  const _transformCharacter = (char) => {
     return {
       id: char.id,
       name: char.name,
@@ -42,6 +44,8 @@ class MarvelService {
       comics: char.comics.items
     }
   }
+
+  return {loading, error, getAllCharacters, getCharacter }
 }
 
-export default MarvelService;
+export default useMarvelService;
