69d1e61c23ceeb513615590b1731c84522ac941e
Daniel <Danil.danek@gmail.com>
2023-04-07 11:45:29 +0200

    404 page added

---
+++ src/components/pages/404.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom';
+
+const Page404 = () => {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <img 
+        src="https://static.vecteezy.com/system/resources/previews/008/255/804/original/page-not-found-error-404-system-updates-uploading-computing-operation-installation-programs-system-maintenance-graffiti-sprayed-page-not-found-error-404-isolated-on-white-background-vector.jpg" 
+        alt="Page not found" 
+        style={{width: 500}}/>
+      <Link 
+        to="/"
+        style={{display: 'block', textAlign: 'center', fontWeight: 'bold', 'fontSize': 38, textDecoration: 'underline', marginTop: 50, color: '#9F0013'}}>
+          Back to main page</Link>
+    </div>
+  )
+}
+
+export default Page404
+
+//How to import image from url in react component
